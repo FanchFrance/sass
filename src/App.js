d1afc56bf241eb8cb961cb3c852f304fed3c5aa6
Fanch <francois.yhuel@hotmail.fr>
2020-12-22 15:25:31 +0100

    add scss

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./img/moino_white.svg";
+import "./sass/main.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="header">
+        <div className="header__logo-box">
+          <img src={logo} alt="Logo" className="header__logo" />
+        </div>
+        <div className="header__text-box">
+          <h1 className="heading-primary">
+            <span className="heading-primary--main">Bretagne</span>
+            <span className="heading-primary--sub">
+              la cote bretonne vu par des cons
+            </span>
+          </h1>
+          <a href="#section-tours" className="btn btn--white btn--animated">
+            Discover our tours
+          </a>
+        </div>
       </header>
+
+      <main>
+        <section className="section-about">
+          <div className="u-center-text u-margin-bottom-8">
+            <h2 className="heading-secondary">
+              exciting tour for adventourus people
+            </h2>
+          </div>
+          <div className="row">
+            <div className="col-1-of-2">text content</div>
+            <div className="col-1-of-2">image contnent</div>
+          </div>
+        </section>
+      </main>
+
+      {/* <section className="grid-test">
+        <div className="row">
+          <div className="col-1-of-2">Col 1 of 2</div>
+          <div className="col-1-of-2">Col 1 of 2</div>
+        </div>
+        <div className="row">
+          <div className="col-1-of-3">Col 1 of 3</div>
+          <div className="col-1-of-3">Col 1 of 3</div>
+          <div className="col-1-of-3">Col 1 of 3</div>
+        </div>
+        <div className="row">
+          <div className="col-1-of-3">Col 1 of 3</div>
+          <div className="col-2-of-3">Col 2 of 3</div>
+        </div>
+        <div className="row">
+          <div className="col-1-of-4">Col 1 of 4</div>
+          <div className="col-1-of-4">Col 1 of 4</div>
+          <div className="col-1-of-4">Col 1 of 4</div>
+          <div className="col-1-of-4">Col 1 of 4</div>
+        </div>
+        <div className="row">
+          <div className="col-1-of-4">Col 1 of 4</div>
+          <div className="col-1-of-4">Col 1 of 4</div>
+          <div className="col-2-of-4">Col 2 of 4</div>
+        </div>
+        <div className="row">
+          <div className="col-1-of-4">Col 1 of 4</div>
+          <div className="col-3-of-4">Col 3 of 4</div>
+        </div>
+      </section> */}
     </div>
   );
 }
